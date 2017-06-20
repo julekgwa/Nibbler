173a2508547ed0e4e3f1389dce536792bef03330
Khuthadzo TSHIKOTSHI <ktshikot@e4r1p2.jnb.42.fr>
2017-06-20 07:57:07 +0200

    collision method

--- main.cpp
+++ main.cpp
@@ -9,67 +9,108 @@
 #include <stdlib.h>
 
 using namespace std;
-typedef struct node {
+typedef struct node
+{
     int data;
     int xLoc, yLoc;
     char c;
     struct node *next;
 } node;
 
-class List {
-private:
+class List
+{
+  private:
     node *head, *tail;
-    int     maxX;
-    int     maxY;
-public:
+    int maxX;
+    int maxY;
+
+  public:
     List();
 
     void createnode(int value, int, int, char);
 
     void display(WINDOW *);
+    bool collide(WINDOW *);
     void setMax(int x, int y);
 };
 
-List::List() {
+List::List()
+{
     this->head = NULL;
     this->tail = NULL;
 }
 
-void    List::setMax(int x, int y)
+void List::setMax(int x, int y)
 {
     this->maxX = x;
     this->maxY = y;
 }
 
-void List::createnode(int value, int xLoc, int yLoc, char c) {
+void List::createnode(int value, int xLoc, int yLoc, char c)
+{
     node *temp = new node;
     temp->data = value;
     temp->xLoc = xLoc;
     temp->c = c;
     temp->yLoc = yLoc;
     temp->next = NULL;
-    if (head == NULL) {
+    if (head == NULL)
+    {
         head = temp;
         tail = temp;
         temp = NULL;
-    } else {
+    }
+    else
+    {
         tail->next = temp;
         tail = temp;
     }
 }
-
-void List::display(WINDOW *win) {
+bool List::collide(WINDOW *win)
+{
     node *tmp = new node;
     tmp = head;
-    while (getch() != 'x') {
-//        cout << tmp->data << "\t";
-//        if (tmp->data == 5) {
-//            createnode(0);
-//        }
+    tmp = tmp->next;
+    //check to see if the snake has collided with the wall.
+    if ((tmp->xLoc >= this->maxX || tmp->yLoc >= this->maxY) || (tmp->yLoc <= 0 || tmp->xLoc <= 0))
+        {
+            mvwprintw(win, 10, 10, "Hit Wall", head->yLoc, maxY, head->xLoc, maxX);
+            wrefresh(win);
+            //slow down the game to allow for viewing of the message.
+            usleep(1e+9 / 100);
+            return (true);
+        }
+    //check if the head of the snake is at the same position as on of its body parts;
+    while (tmp)
+    {
+        if ((tmp->xLoc  == head->xLoc && tmp->yLoc == head->yLoc))
+        {
+            mvwprintw(win, 10, 10, "eat self", head->yLoc, maxY, head->xLoc, maxX);
+            wrefresh(win);
+            //slow down the game to allow for viewing of the message.
+            usleep(1e+9 / 100);
+            return (true);
+        }
+        tmp = tmp->next;
+    }
+    return (false);
+}
+void List::display(WINDOW *win)
+{
+    node *tmp = new node;
+    tmp = head;
+    while (getch() != 'x')
+    {
+        //        cout << tmp->data << "\t";
+        //        if (tmp->data == 5) {
+        //            createnode(0);
+        //        }
         wclear(win);
         wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
-        while (tmp) {
-            if (tmp->next) {
+        while (tmp)
+        {
+            if (tmp->next)
+            {
                 tmp->next->yLoc = tmp->yLoc;
                 tmp->next->xLoc = tmp->xLoc - 1;
             }
@@ -78,52 +119,48 @@ void List::display(WINDOW *win) {
             wrefresh(win);
             tmp = tmp->next;
         }
-
-        if ((head->xLoc >= this->maxX || head->yLoc >= this->maxY) || (head->yLoc == 0 || head->xLoc == 0))
+        //check for collision
+        if (collide(win)){
+            break;
+        }
+        if (tmp == NULL)
         {
-            mvwprintw(win, 10 , 10 , "it works cause Khutz did it...", head->yLoc,maxY, head->xLoc,maxX);
-            wrefresh(win);
-                        usleep(1e+9 / 100);
-            break ;
-        }
-        if (tmp == NULL) {
             tmp = head;
-            tmp->xLoc += 4;
+            tmp->xLoc += 1;
         }
-        //usleep()
     }
     endwin();
 }
 
-
-int main() {
+int main()
+{
     List obj;
-//    obj.createnode(6);
-//    obj.createnode(0);
-//    obj.createnode(0);
-//    obj.createnode(0);
-//    obj.createnode(0);
-//    obj.createnode(0);
-//    obj.display();
+    //    obj.createnode(6);
+    //    obj.createnode(0);
+    //    obj.createnode(0);
+    //    obj.createnode(0);
+    //    obj.createnode(0);
+    //    obj.createnode(0);
+    //    obj.display();
 
     initscr();
     int height, width, starty, startx;
     getmaxyx(stdscr, height, width);
     curs_set(0);
-    starty = (height - (height - 2)) / 2;    /* Calculating for a center placement */
+    starty = (height - (height - 2)) / 2; /* Calculating for a center placement */
     startx = (width - (width - 2)) / 2;
     height -= 2;
     width -= 2;
-    halfdelay(7);
+    halfdelay(1);
     WINDOW *win = newwin(height, width, starty, startx);
     refresh();
     box(win, 0, 0);
     wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
     wrefresh(win);
-    obj.createnode(12, width / 2,  height / 2, 'C');
-    obj.createnode(0,0,0,'@');
-    obj.createnode(0,0,0,'@');
-    obj.createnode(0,0,0,'@');
+    obj.createnode(12, width / 2, height / 2, 'C');
+    obj.createnode(0, 0, 0, '@');
+    obj.createnode(0, 0, 0, '@');
+    obj.createnode(0, 0, 0, '@');
     obj.setMax(width, height);
     obj.display(win);
     endwin();
